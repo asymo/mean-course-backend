8b428d90fb9a33e3fbcae8c6f3386f495cc482b4
Christopher J Symons <cjsymons86@gmail.com>
2018-11-25 09:46:47 +0000

    Added server error logic and api/post path

--- src/app.js
+++ src/app.js
@@ -1,14 +1,24 @@
 const express = require('express');
 
-const app = request();
+const app = express();
 
-app.use((req, res, next) => {
-    console.log('First middleware');
-    next();
-});
-
-app.use((req, res, next) => {
-    res.send('Hello from express!');
+app.use('/api/posts', (req, res, next) => {
+    const posts = [
+        {
+            id: 'sdfghseh98',
+            title: 'First server post',
+            content: 'This is the first post from the server'
+        },
+        {
+            id: 'ngfhtuy38d',
+            title: 'Second server post',
+            content: 'This is the first post from the server!'
+        }
+    ];
+    res.status(200).json({
+        message: 'Posts fetched successfully',
+        posts: posts
+    });
 });
 
 module.exports = app;
