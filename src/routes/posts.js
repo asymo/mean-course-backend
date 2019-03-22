95fcd6da640a4492183e4718c4d8ffb9574bb2d7
Christopher J Symons <cjsymons86@gmail.com>
2019-03-22 10:12:29 +0000

    Updated error messages retured

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -15,7 +15,7 @@ const storage = multer.diskStorage({
     destination: (req, file, callback) => {
         const isValid = MIME_TYPE_MAP[file.mimetype];
         let error = new Error('Invalid mime type');
-        if(isValid) {
+        if (isValid) {
             error = null;
         }
         callback(error, 'images');
@@ -27,7 +27,9 @@ const storage = multer.diskStorage({
     }
 });
 
-router.post('', authCheck, multer({storage: storage}).single('image'), (req, res, next) => {
+router.post('', authCheck, multer({
+    storage: storage
+}).single('image'), (req, res, next) => {
     const url = req.protocol + '://' + req.get('host');
     const post = new Post({
         title: req.body.title,
@@ -36,14 +38,19 @@ router.post('', authCheck, multer({storage: storage}).single('image'), (req, res
         creator: req.userData.userId
     });
     post.save().then(createdPost => {
-    res.status(201).json({
-            message: 'Post added correctly',
-            post: {
-                id: createdPost._id,
-                ...createdPost
-            }
+            res.status(201).json({
+                message: 'Post added correctly',
+                post: {
+                    id: createdPost._id,
+                    ...createdPost
+                }
+            });
+        })
+        .catch(error => {
+            res.status(500).json({
+                message: 'Creating a post failed'
+            });
         });
-    });
 });
 
 router.get('', (req, res, next) => {
@@ -51,27 +58,34 @@ router.get('', (req, res, next) => {
     const currentPage = +req.query.page;
     const postQuery = Post.find();
     let fetchedPosts;
-    if(pageSize && currentPage) {
+    if (pageSize && currentPage) {
         postQuery
             .skip(pageSize * (currentPage - 1))
             .limit(pageSize);
     }
     postQuery.then(documents => {
-        fetchedPosts = documents;
-        return Post.count();
-    })
-    .then(count => {
-        res.status(200).json({
-            message: 'Posts fetched successfully',
-            posts: fetchedPosts,
-            maxPosts: count
+            fetchedPosts = documents;
+            return Post.count();
+        })
+        .then(count => {
+            res.status(200).json({
+                message: 'Posts fetched successfully',
+                posts: fetchedPosts,
+                maxPosts: count
+            });
+        })
+        .catch(error => {
+            res.status(500).json({
+                message: 'Fetching posts failed'
+            });
         });
-    })
 });
 
-router.put('/:id', authCheck, multer({storage: storage}).single('image'), (req, res, next) => {
+router.put('/:id', authCheck, multer({
+    storage: storage
+}).single('image'), (req, res, next) => {
     let imagePath = req.body.imagePath;
-    if(req.file) {
+    if (req.file) {
         const url = req.protocol + '://' + req.get('host');
         imagePath = url + '/images/' + req.file.filename;
     }
@@ -82,33 +96,65 @@ router.put('/:id', authCheck, multer({storage: storage}).single('image'), (req, 
         imagePath: imagePath,
         creator: req.authData.userId
     });
-    Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post)
+    Post.updateOne({
+            _id: req.params.id,
+            creator: req.userData.userId
+        }, post)
         .then(result => {
             if (result.nModified > 0) {
-                res.status(200).json({message: 'Update successful!'});
+                res.status(200).json({
+                    message: 'Update successful!'
+                });
             } else {
-                res.status(401).json({message: 'Not authorised!'});
+                res.status(401).json({
+                    message: 'Not authorised!'
+                });
             }
+        })
+        .catch(error => {
+            res.status(500).json({
+                message: 'Editing a post failed'
+            });
         });
 });
 
 router.get('/:id', (req, res, next) => {
-    Post.findById(req.params.id).then(post => {
-        if (post) {
-            res.status(200).json(post);
-        } else {
-            res.status(404).json({message: 'Post not found!'});
-        }
-    });
+    Post.findById(req.params.id)
+        .then(post => {
+            if (post) {
+                res.status(200).json(post);
+            } else {
+                res.status(404).json({
+                    message: 'Post not found!'
+                });
+            }
+        })
+        .catch(error => {
+            res.status(500).json({
+                message: 'Fetching post failed'
+            });
+        });
 });
 
 router.delete('/:id', authCheck, (req, res, next) => {
-    Post.deleteOne({ _id: req.params.id, creator: req.authData.userId }).then(result => {
+    Post.deleteOne({
+        _id: req.params.id,
+        creator: req.authData.userId
+    }).then(result => {
         if (result.n > 0) {
-            res.status(200).json({message: 'Deletion successful!'});
+            res.status(200).json({
+                message: 'Deletion successful!'
+            });
         } else {
-            res.status(401).json({message: 'Not authorised!'});
+            res.status(401).json({
+                message: 'Not authorised!'
+            });
         }
+    })
+    .catch(error => {
+        res.status(500).json({
+            message: 'Deleting a post failed'
+        });
     });
 });
 
